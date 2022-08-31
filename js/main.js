7f1f8cd0ea3dde1ef8f56f86a6e8b96f455d5748
Shreyansh-Jain021 <shreyansh0676.be20@chitkara.edu.in>
2022-08-31 09:49:25 +0530

    ST_2_Completed

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// var navLinks = document.getElementById("navLinks");
+function showMenu() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.style.right = "0";
+}
+function hideMenu() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.style.right = "-200px";
+}
